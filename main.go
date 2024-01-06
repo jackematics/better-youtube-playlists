6dba00e65bc87a5e6c0d530a35be4145d63f8b73
Jack Rimmer <jack.j.rimmer@bbc.co.uk>
2024-01-06 12:05:12 +0000

    Got templ working after a lot of tinkering

--- main.go
+++ main.go
@@ -3,32 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"text/template"
 
-	"github.com/gorilla/mux"
+	"github.com/a-h/templ"
+	"github.com/jackematics/better-youtube-playlists/templates"
 )
 
-var templates = template.Must(template.ParseGlob("templates/*"))
-
 func main() {
-	router := mux.NewRouter()
-
-	router.HandleFunc("/", homeHandler).Methods("GET")
+	index := templates.Index()
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	http.Handle("/", router)
+
+	http.Handle("/", templ.Handler(index))
 
 	fmt.Println("Server started on :8000")
 	http.ListenAndServe(":8000", nil)
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", data)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "index", nil)
 }
