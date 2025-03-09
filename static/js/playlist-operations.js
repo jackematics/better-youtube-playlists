67be4701d42decc94d26a205efd3baed3532a44b
Jackematics <jrimmer2996@gmail.com>
2025-03-09 19:15:03 +0000

    handles next button

--- static/js/playlist-operations.js
+++ static/js/playlist-operations.js
@@ -1,5 +1,3 @@
-const previous = document.getElementById("previous");
-
 function handlePreviousClick() {
   const prevVideo = document
     .getElementById("playlist-items")
@@ -10,4 +8,18 @@ function handlePreviousClick() {
   }
 }
 
+function handleNextClick() {
+  const nextVideo = document
+    .getElementById("playlist-items")
+    .querySelector(".bg-warm-orange").nextElementSibling;
+
+  if (nextVideo) {
+    nextVideo.click();
+  }
+}
+
+const previous = document.getElementById("previous");
 previous.addEventListener("click", handlePreviousClick);
+
+const next = document.getElementById("next");
+next.addEventListener("click", handleNextClick);
